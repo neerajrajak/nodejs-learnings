02ad2f85a671ee28403e28e7604d482ce5173441
neerajrajak <neeraj92.rajak@gmail.com>
2024-11-20 15:40:59 +0530

    api validation

--- devtinder-backend/src/models/user.js
+++ devtinder-backend/src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema({
   firstName: {
@@ -16,10 +17,20 @@ const userSchema = new mongoose.Schema({
     unique: true,
     lowercase: true,
     trim: true,
+    validate(value){
+        if(!validator.isEmail(value)){
+            throw new Error('Invalid Email Address');
+        }
+    }
   },
   password: {
     type: String,
     required: true,
+    validate(value){
+        if(!validator.isStrongPassword(value)){
+            throw new Error('Password is not strong.');
+        }
+    }
   },
   age: {
     type: Number,
@@ -38,6 +49,11 @@ const userSchema = new mongoose.Schema({
     type: String,
     default:
       "https://cdn.vectorstock.com/i/1000v/92/16/default-profile-picture-avatar-user-icon-vector-46389216.jpg",
+      validate(value){
+        if(!validator.isURL(value)){
+            throw new Error('Invalid Photo URL');
+        }
+    }
   },
   about: {
     type: String,
--- devtinder-backend/src/mongooseExample.js
+++ devtinder-backend/src/mongooseExample.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/signup', async (req, res)=>{
+app.post("/signup", async (req, res) => {
   // creating a new instance of a usermodel
   const user = new User(req.body);
 
@@ -15,69 +15,84 @@ app.post('/signup', async (req, res)=>{
     console.log("User Saved.");
   } catch (error) {
     console.log("An error occured: ", error);
-    
+
     res.status(500).send(error.message);
   }
 });
 
-app.get('/user', async (req, res)=>{
+app.get("/user", async (req, res) => {
   const email = req.body.email;
-  try{
+  try {
     const user = await User.findOne({
-      emailId: email
+      emailId: email,
     });
-    if(!user){
-      res.status(404).send('User not found in the system.');
+    if (!user) {
+      res.status(404).send("User not found in the system.");
     }
-    console.log('User found: ', user);
+    console.log("User found: ", user);
     res.send(user);
-  } catch(err){
+  } catch (err) {
     res.status(500).send("Something went wrong: ", error);
   }
 });
 
-app.get('/feed', async (req, res)=>{
-  try{
+app.get("/feed", async (req, res) => {
+  try {
     const users = await User.find();
-    if(!users.length){
-      res.status(404).send('User not found in the system.');
+    if (!users.length) {
+      res.status(404).send("User not found in the system.");
     }
-    console.log('User found: ', users);
+    console.log("User found: ", users);
     res.send(users);
-  } catch(err){
+  } catch (err) {
     res.status(500).send("Something went wrong: ", error);
   }
 });
 
-app.delete('/user', async (req, res)=>{
+app.delete("/user", async (req, res) => {
   const userId = req.body.userId;
   try {
     const user = await User.findByIdAndDelete(userId);
     console.log("Deleted: ", user);
-    if(user){
+    if (user) {
       res.send("User deleted.");
-    } else{
-      res.status(404).send('User not found to delete')
+    } else {
+      res.status(404).send("User not found to delete");
     }
-  } catch(err){
+  } catch (err) {
     res.status(500).send("Something went wrong: ", error);
   }
 });
 
-app.patch('/user', async (req, res)=>{
-  const userId = req.body.userId;
+app.patch("/user/:userId", async (req, res) => {
+  const userId = req.params?.userId;
   const data = req.body;
-  
+  const ALLOWED_UPDATES = ["photoUrl", "about", "age", "skills"];
+  const isUpdateAllowed = Object.keys(data).every((k) =>
+    ALLOWED_UPDATES.includes(k)
+  );
+  console.log("isUpdateAllowed: ", data);
+  if (data?.skills?.length > 10) {
+    res.status(400).send(`Max 10 skills are allowed`);
+  }
+  if (!isUpdateAllowed) {
+    res
+      .status(400)
+      .send(`Only ${ALLOWED_UPDATES.join(",")} are allowed to update`);
+  }
   try {
-    const user = await User.findByIdAndUpdate(userId, data, { returnDocument: 'after', runValidators: true});
+    const user = await User.findByIdAndUpdate(userId, data, {
+      returnDocument: "after",
+      runValidators: true,
+    });
     console.log("Updated User: ", user);
-    if(user){
+    if (user) {
       res.send(user);
-    } else{
-      res.status(404).send('User not found to update')
+    } else {
+      res.status(404).send("User not found to update");
     }
-  } catch(err){
-    res.status(500).send('UPDATE Failed: '+err.message);
+  } catch (err) {
+    res.status(500).send("UPDATE Failed: " + err.message);
   }
 });
 
